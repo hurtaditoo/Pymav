a5cb69df76badcc74414bd55ec1525c64dc273f1
Irene Mejía <113619628+hurtaditoo@users.noreply.github.com>
2025-05-11 17:24:53 +0200

    Delete build directory

--- build/pymav/CMakeFiles/pymav_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/pymav_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/pymav_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
